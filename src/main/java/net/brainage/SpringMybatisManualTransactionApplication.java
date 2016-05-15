9dabf2c55aeaf1b88d4284e80c2d0b6543b0ea9e
ms29.seo@gmail.com <ms29.seo@gmail.com>
2016-05-16 02:02:39 +0900

    create spring boot + mybatis-spring boot maven project

---
+++ src/main/java/net/brainage/SpringMybatisManualTransactionApplication.java
@@ -0,0 +1,12 @@
+package net.brainage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisManualTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisManualTransactionApplication.class, args);
+	}
+}
---
+++ src/test/java/net/brainage/SpringMybatisManualTransactionApplicationTests.java
@@ -0,0 +1,16 @@
+package net.brainage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringMybatisManualTransactionApplication.class)
+public class SpringMybatisManualTransactionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
